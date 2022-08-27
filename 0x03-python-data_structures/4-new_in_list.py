8056d3161607d9227b175907fea9c8dff776f33b
oyin-bam <oyindaakinyemi@gmail.com>
2022-08-27 15:57:39 -0700

    returning a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    check_list = my_list.copy()
     if idx < 0:
-        return my_list
+        return check_list
     elif idx > (len(my_list) - 1):
-        return my_list
+        return check_list
     else:
-        my_list[idx] = element
-        return my_list
+        check_list[idx] = element
+        return checklist_list
