62280379e273b95358d53fcf4873a6159fc2ac6e
oyin-bam <oyindaakinyemi@gmail.com>
2022-08-27 09:59:02 -0700

    changed format

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    num_of_arg = len(sys.argv)
-    if num_of_arg == 1:
-        print("{} arguments.".format(num_of_arg - 1))
-    elif num_of_arg == 2:
-        print("{} argument:".format(num_of_arg - 1))
+    num = len(sys.argv)
+    if num == 1:
+        print("{} arguments.".format(num - 1))
+    elif num == 2:
+        print("{} argument:".format(num - 1))
     else:
-        print("{} arguments:".format(num_of_arg - 1))
+        print("{} arguments:".format(num - 1))
 
-    for i in range(1, num_of_arg):
+    for i in range(1, num):
         print("{}: {}".format(i, sys.argv[i]))
