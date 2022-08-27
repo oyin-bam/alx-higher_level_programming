fb2825fb8273360f5b8191ecd813d7dfc78bedbf
oyin-bam <oyindaakinyemi@gmail.com>
2022-08-27 14:23:33 -0700

    refactoring

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from hidden_4 import *
-    for i in range(len(dir())):
-        if dir()[i][0:2] != "__":
+    names = dir()
+    for i in range(len(names)):
+        if names[i][0:2] != "__":
             print("{}".format(dir()[i]))
