714363104a456205795944cb388ce78c5bff308a
oyin-bam <oyindaakinyemi@gmail.com>
2022-08-27 09:56:39 -0700

    corrected print format

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     elif num_of_arg == 2:
         print("{} argument:".format(num_of_arg - 1))
     else:
-        print("{} arguments.".format(num_of_arg - 1))
+        print("{} arguments:".format(num_of_arg - 1))
 
     for i in range(1, num_of_arg):
         print("{}: {}".format(i, sys.argv[i]))
